1f5074bbcd6f8f71cfacb2a7226d456bcbd43f25
Neptune <91170034+ChinaNereus@users.noreply.github.com>
2025-06-06 10:39:51 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,7 +31,7 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "ikunikun"         # 用户名
+USER_NAME = "ikun"         # 用户名
 UUID = "c9c7f1fe-d5ba-4083-951a-c81c0a03adf7"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
 DOMAIN = "streamlit.ikun.us.kg"                   # 域名，留空则自动获取
